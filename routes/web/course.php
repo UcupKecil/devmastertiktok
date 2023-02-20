6a069fb0262aac0a7498afd85bfa602ff7094844
ucupkecil <dhitapinan@gmail.com>
2023-02-20 17:37:33 +0700

    route course

--- routes/web/course.php
+++ routes/web/course.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Http\Controllers\CourseVideoController;
 use App\Http\Controllers\StaticPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/course/{slug}', [StaticPageController::class, 'course']);
+
+
+Route::group([
+    'middleware'    => ['auth', 'role:Member'],
+], function () {
+    Route::get('/mycourse/change-video/{class_id}/{id}', [CourseVideoController::class, 'changeVideo']);
+    Route::get('/mycourse/{slug}', [StaticPageController::class, 'classroom']);
+});

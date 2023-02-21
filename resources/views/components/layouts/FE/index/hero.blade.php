e397eea477a1e627dce1b67859a46ea04dba1a83
ucupkecil <dhitapinan@gmail.com>
2023-02-21 10:57:56 +0700

    test

--- resources/views/components/layouts/FE/index/hero.blade.php
+++ resources/views/components/layouts/FE/index/hero.blade.php
@@ -1,14 +1,10 @@
 <section class="hero-area" style="background-image: url({{ asset('/assets/images/settings/' . $setting->imagebanner) }});">
     <div class="container">
         <div class="caption-content text-center">
-            <h4>Start learning from home</h4>
-            <h2>Connect With Our Expert And Start Learning Today</h2>
-            <p>We are providing high-quality online courses to improve your skill. Our all instructors are highly
-                experienced and experts.</p>
-            <ul>
-                <li><a href="#">Find courses</a></li>
-                <li><a class="btn-bg" href="#">Start free trial</a></li>
-            </ul>
+            <h4>MULAI BELAJAR DARI RUMAH</h4>
+            <h2>Terhubung Dengan Pakar Kami Dan Mulai Belajar Sekarang</h2>
+            <p>Kami menyediakan kursus online berkualitas tinggi untuk meningkatkan keterampilan Anda. Semua instruktur kami sangat berpengalaman dan ahli.</p>
+            
         </div>
     </div>
 </section>

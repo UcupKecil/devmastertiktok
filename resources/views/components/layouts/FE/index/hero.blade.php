8267dba82eee138e3b2b3e6c75625f119f6b8e4f
ucupkecil <dhitapinan@gmail.com>
2023-03-04 15:44:50 +0700

    typo hero

--- resources/views/components/layouts/FE/index/hero.blade.php
+++ resources/views/components/layouts/FE/index/hero.blade.php
@@ -2,8 +2,8 @@
     <div class="container">
         <div class="caption-content text-center">
             <h4>Master Tiktok Agency - Online Class</h4>
-            <h4 style="color:#fff;">Dikelas ini Akan duajarkan semua cara kami mendapatkan puluhan juta rupiah dari tiktok dari Nol sampai bisa</h4>
-            <p>kelas ini hanya untuk yang serius dan ingin punya penghasilan tambahan, karna semuanya akan diajarkan dari nol cuma modal HP dan kuota saja tanpa modal tambahan. Silahkan daftar sekarang sebelum kelas ini ditutup</p>
+            <h4 style="color:#fff;">Dikelas ini Akan diajarkan semua cara kami mendapatkan puluhan juta rupiah dari tiktok dari Nol sampai bisa</h4>
+            <p>kelas ini hanya untuk yang serius dan ingin punya penghasilan tambahan, karena semuanya akan diajarkan dari nol cuma modal HP dan kuota saja tanpa modal tambahan. Silahkan daftar sekarang sebelum kelas ini ditutup</p>
             
         </div>
     </div>
